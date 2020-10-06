0ffb8d8225bda8a7ae45a0520cef18fed2ca8257
turboguy <turboguy36@gmail.com>
2020-10-06 20:24:06 +0900

    good luck

--- src/lee/hyoung/gu/FirstClass.java
+++ src/lee/hyoung/gu/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world"); // sysout => ctrl + space
+		System.out.println("good luck!!");
 	}
 
 }
