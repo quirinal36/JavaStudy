2ef87ded4d2b64a6f8a0f4d7b51afd982d416d27
turboguy <turboguy36@gmail.com>
2020-10-06 20:15:22 +0900

    init

---
+++ src/lee/hyoung/gu/FirstClass.java
@@ -0,0 +1,10 @@
+package lee.hyoung.gu;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world"); // sysout => ctrl + space
+	}
+
+}
