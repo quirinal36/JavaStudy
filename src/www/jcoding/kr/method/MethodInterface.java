6c4a029e44254eb1792c7a35dc7117f2fbc33322
turboguy <turboguy36@gmail.com>
2020-10-08 17:42:01 +0900

    interface method

---
+++ src/www/jcoding/kr/method/MethodInterface.java
@@ -0,0 +1,12 @@
+package www.jcoding.kr.method;
+
+public interface MethodInterface {
+	public int sum(int[] arr);
+	public double average(int[] arr);
+	public int max(int[] arr);
+	public int min(int[] arr);
+	
+	public String left(String input, int num);
+	public String rignt(String input, int num);
+	public String mid(String input, int start, int num);
+}
---
+++ src/www/jcoding/kr/method/Practice.java
@@ -0,0 +1,47 @@
+package www.jcoding.kr.method;
+
+public class Practice implements MethodInterface{
+
+	@Override
+	public int sum(int[] arr) {
+		// TODO arr 안의 모든 값들의 합계를 계산해 return 한다.
+		return 0;
+	}
+
+	@Override
+	public double average(int[] arr) {
+		// TODO arr 안의 모든 값들의 평균을 계산해 return 한다.
+		return 0;
+	}
+
+	@Override
+	public int max(int[] arr) {
+		// TODO arr 안의 모든 값들 중 가장 큰 값을 찾아 return 한다.
+		return 0;
+	}
+
+	@Override
+	public int min(int[] arr) {
+		// TODO arr 안의 모든 값들 중 가장 작은 값을 찾아 return 한다.
+		return 0;
+	}
+
+	@Override
+	public String left(String input, int num) {
+		// TODO input 의 첫 글자 부터 num 개의 글자를 return 한다.
+		return null;
+	}
+
+	@Override
+	public String rignt(String input, int num) {
+		// TODO input 의 마지막 글자 부터 num 개의 글자를 return 한다.
+		return null;
+	}
+
+	@Override
+	public String mid(String input, int start, int num) {
+		// TODO input 의 start 번째 글자부터 num 개의 글자를 return 한다.
+		return null;
+	}
+
+}
