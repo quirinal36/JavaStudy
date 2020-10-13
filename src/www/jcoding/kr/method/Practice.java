e9d9e3fc4e578b492300e97ef4b3c5d4600beb47
turboguy <turboguy36@gmail.com>
2020-10-13 20:22:35 +0900

    내려받아주세요.

--- src/www/jcoding/kr/method/Practice.java
+++ src/www/jcoding/kr/method/Practice.java
@@ -15,61 +15,65 @@ public class Practice implements MethodInterface{
 	@Override
 	public double average(int[] arr) {
 		// TODO arr 안의 모든 값들의 평균을 계산해 return 한다.
-		return 0;
+		double avg = sum(arr) / arr.length;
+		return avg;
 	}
 
 	@Override
 	public int max(int[] arr) {
 		// TODO arr 안의 모든 값들 중 가장 큰 값을 찾아 return 한다.
-		return 0;
+		int result = 0;
+		for(int n : arr) {
+			if(n > result) {
+				result = n;
+			}
+		}
+		return result;
 	}
 
 	@Override
 	public int min(int[] arr) {
 		// TODO arr 안의 모든 값들 중 가장 작은 값을 찾아 return 한다.
-		return 0;
+		int result = max(arr);
+		for(int n : arr) {
+			if(n < result) {
+				result = n;
+			}
+		}
+		return result;
 	}
 
 	@Override
 	public String left(String input, int num) {
 		// TODO input 의 첫 글자 부터 num 개의 글자를 return 한다.
-		return null;
+		String result;
+		result = input.substring(0, num);
+		return result;
 	}
-
+	
 	@Override
 	public String right(String input, int num) {
 		// TODO input 의 마지막 글자 부터 num 개의 글자를 return 한다.
-		return null;
+		String result = input.substring(input.length() - num, input.length());
+		return result;
 	}
-
+	
+	
 	@Override
 	public String mid(String input, int start, int num) {
 		// TODO input 의 start 번째 글자부터 num 개의 글자를 return 한다.
-		return null;
+		return input.substring(start, start+num);
 	}
 	
 	public static void main(String[] args) {
-		Practice p = new Practice();
-		
-		int[] data = {50, 60, 70, 80, 90};
-		String phoneNum = "010-5679-0072";
-		
-		int sumResult = p.sum(data);
-		System.out.println("합계: " + sumResult);
-		
-		double avg = p.average(data);
-		System.out.println("평균: " + avg);
-		
-		System.out.println("최댓값: " + p.max(data));
-		System.out.println("최소값: " + p.min(data));
-		
-		String phone01 = p.left(phoneNum, 3);
-		System.out.println("처음 세글자: " + phone01);
-		
-		String phone02 = p.mid(phoneNum, 5, 4);
-		System.out.println("가운데 네글자: " + phone02);
-		
-		String phone03 = p.right(phoneNum, 4);
-		System.out.println("마지막 네글자: " + phone03);
+		// continue 와 break
+		// continue : 계속하다
+		// break : 멈추다
+		for(int i=0; i<10; i++) {
+			if(i == 5) {
+				break;
+			}
+			System.out.println(i);
+		}
 	}
 }
