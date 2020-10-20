b0b76af00fe8a4da4a48965b189e63a76157ac3d
akzmwhgdk <akzmwhgdk@DESKTOP-FDRV89N.mshome.net>
2020-10-20 20:31:17 +0900

    a

---
+++ src/www/jcoding/kr/minigold649/Solution01.java
@@ -0,0 +1,32 @@
+package www.jcoding.kr.minigold649;
+
+import javax.swing.JOptionPane;
+import java.util.*;
+
+public class Solution01 {
+	public int[] solution(int[] input) {
+		int[] result = new int[input.length - 1];
+		
+		return result;
+	}
+	
+	public int[] getInput() {
+		// 다이얼로그를 띄워 입력받는다.
+		String numberStr = JOptionPane.showInputDialog("숫자를 몇 개 입력하시겠습니까?");
+		int number = Integer.parseInt(numberStr);
+		int arr[] = new int[number];
+		
+		Scanner sc = new Scanner(System.in);
+		int i=0;
+		while(number > 0) {
+			number--;
+			System.out.print("please input>>");
+			int inputNum = sc.nextInt();
+			arr[i++] = inputNum;
+		}
+		
+		sc.close();
+		
+		return arr;
+	}
+}
