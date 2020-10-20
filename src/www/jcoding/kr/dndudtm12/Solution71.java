2bb157d81622a29a8fdf14ee0893ab8af095bc2b
강우영 <@DESKTOP-92B6AE3>
2020-10-20 20:35:28 +0900

    1020

---
+++ src/www/jcoding/kr/dndudtm12/Solution71.java
@@ -0,0 +1,27 @@
+package www.jcoding.kr.dndudtm12;
+
+import java.util.Scanner;
+
+public class Solution71 {
+	public int solution(int n, int m) {
+		int result = 0;
+		result = func_a(m) - func_a(n-1);
+		return result;
+	}
+	
+	public int func_a(int num) {
+		int sum = 0;
+		// sum : 1 부터 num 까지의 합계 
+		return sum;
+	}
+	
+	public int getInput() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("please input number >>");
+		int input = sc.nextInt();
+		
+		sc.close();
+		
+		return input;
+	}
+}
