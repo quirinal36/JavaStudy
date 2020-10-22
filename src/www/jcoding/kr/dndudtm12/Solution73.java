7845b9f709a4fb4e0907ac8bce86a59b00819913
강우영 <@DESKTOP-92B6AE3>
2020-10-22 20:47:23 +0900

    1022

--- src/www/jcoding/kr/dndudtm12/Solution73.java
+++ src/www/jcoding/kr/dndudtm12/Solution73.java
@@ -17,7 +17,7 @@ public class Solution73 {
 		// TODO : 전달받은 배열에서 가장 큰 수를 찾습니다.
 		int n = 0;
 		for(n=0; n < arr.length; n++) {
-			if(arr[n]>result) {
+			if(arr[n] > result) {
 				result = arr[n];
 			}
 		}
@@ -27,9 +27,10 @@ public class Solution73 {
 	public int[] exceptMax(int[] arr, int maxValue) {
 		int[] result = new int[arr.length-1];
 		// TODO : result 배열을 가장 큰 값(maxValue)를 제외한 배열로 만들어 주세요.
-		for(int n = 0;n < result.length; n++) {
-			if(result[n] != maxValue) {
-				result[n] = arr[n];
+		int i = 0;
+		for(int n = 0;n < arr.length; n++) {
+			if(arr[n] != maxValue) {
+				result[i++] = arr[n];
 			}
 		}
 		return result;
