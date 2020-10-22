2255420903997d2bdff644199d3d8f5523c909f7
nana2365 <nana2365@DESKTOP-CQMR31M>
2020-10-22 21:00:28 +0900

    201022

--- src/www/jcoding/kr/nana2365/Solution72.java
+++ src/www/jcoding/kr/nana2365/Solution72.java
@@ -3,12 +3,11 @@ package www.jcoding.kr.nana2365;
 public class Solution72 {
 	public int getMax(int[] arr) {
 		int result = 0;
-		int max=arr[0];
-		
+				
 		// TODO: 배열에 들어있는 값들 중 가장 큰 점수를 골라주세요.
 
 		for(int i=0; i<arr.length; i++){
-		    if(arr[i] > max){
+		    if(arr[i] > result){
 		       result = arr[i];
 		      }
 		   }
@@ -16,12 +15,11 @@ public class Solution72 {
 	}
 	
 	public int getMin(int[] arr) {
-		int result = 0;
-		int min = arr[0];
+		int result =getMax(arr);
 		// TODO: 배열에 들어있는 값들 중 가장 작은 점수를 골라주세요.
 		
 		for(int i=0; i<arr.length; i++){
-		    if(arr[i] < min){
+		    if(arr[i] < result){
 		       result = arr[i];
 		      }
 		   }
@@ -32,10 +30,13 @@ public class Solution72 {
 		int result = 0;
 		int max = getMax(arr);
 		int min = getMin(arr);
-		
+		System.out.println("max:"+max);
+		System.out.println("min:"+min);
+	
 		// TODO: 가장 높은점수(max)와 가장 낮은점수(min)을 제외한 나머지 점수들의 합을 구하세요.
+	
 		for(int i =0; i<arr.length; i++) {
-			if(arr[i]>max && arr[i]>min) {
+			if(arr[i]<max && arr[i]>min) {
 				result = result + arr[i];
 			}
 		}
