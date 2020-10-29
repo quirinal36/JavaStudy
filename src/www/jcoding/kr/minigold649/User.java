83bc426928e78c71d056bed507e2585a0a46b22b
turboguy <turboguy36@gmail.com>
2020-10-29 18:41:11 +0900

    은표 패키지로 이동

---
+++ src/www/jcoding/kr/minigold649/User.java
@@ -0,0 +1,24 @@
+package www.jcoding.kr.minigold649;
+
+import java.util.Date;
+
+public class User {
+	int age;
+	Date birth;
+	String login;
+	String password;
+	String password_confirm;
+	String username;
+	String phone;
+	String email;
+	
+	
+	@Override
+	public String toString() {
+		return "login: " + this.login +
+				"\npassword: " + this.password +
+				"\nusername: " + this.username +
+				"\nphone: " + this.phone +
+				"\nemail: " + this.email;
+	}
+}
